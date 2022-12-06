4cf5eab6e3fe85dffecf3a6b17968cf39b0a34bd
Sarkhan <abdullayevsarxan92@gmail.com>
2022-12-06 22:54:49 +0400

    feat: adding new functional to timer

--- js/script.js
+++ js/script.js
@@ -42,17 +42,27 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //timer
 
-    const deadline = '2022-12-21'; // input с type data возвращает с таким форматом так что лучше привыкать так
+    const deadline = '2022-12-20'; // input с type data возвращает с таким форматом так что лучше привыкать так
     // миллисекунда 1 / 1000 , в 1 сек = 60000
     function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+
         // get difference between dates
-        const t = Date.parse(endtime) - Date.parse(new Date()), // количество милли секунд получаем тоже самое что new Date()
-            // можно наверху new Date использовать и без parse
-            days = Math.floor(t / (1000 * 60 * 60 * 24)), // Math.floor - это округление до ближащего целого
-            hours = Math.floor(((t / 1000) * 60 * 60) % 24), // первое скобку общее количество часов получаем
-            //а потом делим на 24 и получаем остатку// 50 hours % 24 и вернет всего 2 часа
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            seconds = Math.floor((t / 1000) % 60);
+        const t = Date.parse(endtime) - Date.parse(new Date()); // количество милли секунд получаем тоже самое что new Date()
+        // можно наверху new Date использовать и без parse
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            (days = Math.floor(t / (1000 * 60 * 60 * 24))), // Math.floor - это округление до ближащего целого
+                (hours = Math.floor(((t / 1000) * 60 * 60) % 24)), // первое скобку общее количество часов получаем
+                //а потом делим на 24 и получаем остатку// 50 hours % 24 и вернет всего 2 часа
+                (minutes = Math.floor((t / 1000 / 60) % 60)),
+                (seconds = Math.floor((t / 1000) % 60));
+        }
 
         return {
             // чтобы вернуть наружу
