6ab3420cba2a44cb807948e079dccaa015fe52f4
Sarkhan <abdullayevsarxan92@gmail.com>
2022-12-23 20:42:41 +0400

    refactor: fetch api is applied

--- js/script.js
+++ js/script.js
@@ -298,52 +298,74 @@ window.addEventListener('DOMContentLoaded', () => {
             statusMessage.src = message.loading;
             statusMessage.style.cssText = `
             display: block;
-            margin: 0 auto`;
+            margin: 0 auto;`;
 
             // statusMessage.textContent = message.loading;
             // form.append(statusMessage);1 WAY
             // 2 WAY of adding element to page
 
             form.insertAdjacentElement('afterend', statusMessage);
-            const request = new XMLHttpRequest();
 
-            request.open('POST', 'server.php');
+            // const request = new XMLHttpRequest();
+
+            // request.open('POST', 'server.php');
+
             // request.setRequestHeader(
             //     'Content-type',
             //     'application/json; charset=utf-8'
             // ); when use formdata and xmlrequest header we should not use
             // а если наш сервер принимает в json формете то тогда нужет заголовок
-            request.setRequestHeader('Content-type', 'application/json');
-            const object = {};
-
-            const formData = new FormData(form); // автоматически будет взять с форма все данные
-            // чтобы formDATA сработала надо в верстке обязательно указать name"phone or Name"
+            // request.setRequestHeader('Content-type', 'application/json');
 
             // в консоли можео делать и медленное 3 g
-
+            const formData = new FormData(form); // автоматически будет взять с форма все данные
+            // чтобы formDATA сработала надо в верстке обязательно указать name"phone or Name"
+            const object = {};
             formData.forEach(function (value, key) {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object); // превращает обычный объект в json
+            // const json = JSON.stringify(object); // превращает обычный объект в json
 
             // request.send(formData);
-            request.send(json); // comparison the demand of our servis
+            // request.send(json); // comparison the demand of our servis
 
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    // statusMessage.textContent = message.success;
+            // request.addEventListener('load', () => {
+            //     if (request.status === 200) {
+            //         console.log(request.response);
+            //         // statusMessage.textContent = message.success;
+            //         showThanksModal(message.success);
+            //         form.reset(); // чтобы очистить форму.. альтернатив брать инпуты перебрать их и очистить их value
+            //         setTimeout(() => {
+            //             statusMessage.remove();
+            //         }, 2000);
+            //     } else {
+            //         // statusMessage = message.failure;
+            //         showThanksModal(message.failure);
+            //     }
+            // });
+
+            fetch('server.php', {
+                // если адрес указан не правильно cathc (reject) не будет выполняться. а при отключение интернета будет выполныяться
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(object),
+            })
+                // .then((data) => data.text()) // превращаем в обычные текст
+                .then((data) => {
+                    console.log(data); // data те данные которые возвращаются с promisess
                     showThanksModal(message.success);
-                    form.reset(); // чтобы очистить форму.. альтернатив брать инпуты перебрать их и очистить их value
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
-                } else {
-                    // statusMessage = message.failure;
+
+                    statusMessage.remove();
+                })
+                .catch(() => {
                     showThanksModal(message.failure);
-                }
-            });
+                })
+                .finally(() => {
+                    form.reset();
+                });
         });
     }
 
